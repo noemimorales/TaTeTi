44fd7775a23d63e80223c98af1ecb1de5b27c0e4
noemimorales <79290160+noemimorales@users.noreply.github.com>
2021-08-28 12:29:54 -0300

    Initial commit

---
+++ Assets/Scripts/View/TatetiView.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TatetiView : MonoBehaviour
+{
+    [SerializeField] private Button[] inputButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
